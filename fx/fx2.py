14ce385e89453266e4ef3ad26a04a42b475197aa
theihor <the.ihor@gmail.com>
2015-12-08 22:29:19 +0200

    Playing with pixels

--- fx/fx2.py
+++ fx/fx2.py
@@ -14,63 +14,16 @@ SIZE = (600, 600)
 
 # create window
 window = pygame.display.set_mode(SIZE)
-pygame.display.set_caption('Citadels')
-GLOBAL_FPS = 60
+GLOBAL_FPS = 60 
 COLOR_WHITE = (255, 255, 255)
 COLOR_BLACK = (0,0,0)
 
 def load_image(name, colorkey=None):
-    fullname = os.path.join('img', name)
+    fullname = name
     image = pygame.image.load(fullname)
-    image = image.convert()
-    if colorkey is not None:
-        if colorkey is -1:
-            colorkey = image.get_at((0,0))
-            image.set_colorkey(colorkey, RLEACCEL)
+    image = image.convert_alpha()
     return image
 
-
-class Card(Sprite):
-    def __init__(self, img_file_name, x=0, y=0):
-        Sprite.__init__(self)
-        self.pos = (x, y)
-        self.source_img = Surface((200, 300))
-        self.source_img.fill(COLOR_BLACK)
-        self.source_img.convert_alpha()
-        #self.source_img.set_alpha(100)
-        self.img = self.source_img
-        self.picked = False
-
-    def set_pos(self, x, y):
-        self.pos = (x, y)
-
-    def move(self, vector):
-        (dx, dy) = vector
-        (x, y) = self.pos
-        self.set_pos(x + dx, y + dy)
-
-    def scale_d(self, diff=(0, 0)):
-        (dx, dy) = diff
-        size = (self.get_rect().w + dx, self.get_rect().h + dy)
-        self.img = pygame.transform.smoothscale(self.source_img, size)
-
-    def scale(self, w, h):
-        size = (w, h)
-        self.img = pygame.transform.smoothscale(self.source_img, size)
-
-    def reset_img(self):
-        self.img = self.source_img
-
-    def set_picked(self, picked=True):
-        self.picked = picked
-
-    def get_rect(self):
-        r = self.img.get_rect()
-        (r.x, r.y) = self.pos
-        return r
-
-the_card = Card('card2.bmp', 50, 300)
-the_card.set_pos(100, 100)
 clock = pygame.time.Clock()
 
 
@@ -80,13 +33,11 @@ def effect_step(img):
     pass
 
 img = Surface((200, 300), pygame.SRCALPHA)
+img = load_image("button.png")
+#img.fill(COLOR_BLACK)
 #img.convert_alpha()
 #img.lock()
 print(img.get_masks())
-
-# for x in range(img.get_rect().h // 2):
-#     for y in range(img.get_rect().h // 2):
-#         img.set_at((x,y), (0,128,0,128))
 
 
 
@@ -104,19 +55,58 @@ def refresh_scene(card):
 
 
 f = True
-while 1:
+
+def effect_step(img):
+    global f
     a = pixels_alpha(img)
 
-    if a[0][0] == 255: f = False
-    if a[0][0] == 0: f = True
-    if f: a += 1
-    else: a -= 1
-    a = None
-    img.unlock()
+   # j = len(a[0]) // 2
+   # for i in range(5, len(a) - 5):
+   #     if j > 5 and j < len(a[i]) - 5:
+   #         p = 0.2
+   #         if random() < p:
+   #             j += randrange(3) - 1 
+   #         a[i][j] = 0
 
+    img2 = Surface(img.get_rect().size, pygame.SRCALPHA)
+    img2.fill((0, 0, 255))
+    b = pixels_alpha(img2)
+
+    for i in range(len(a)):
+        b[i][0] = 0
+        b[i][len(a[i]) -1] = 0
+
+    for i in range(1, len(a) - 1):
+        for j in range(1, len(a[i]) - 1):
+            if a[i][j] < 20:
+                x = max(a[i-1][j-1], a[i-1][j], a[i-1][j+1],
+                        a[i][j-1], a[i][j], a[i][j+1],
+                        a[i+1][j-1], a[i+1][j], a[i+1][j+1])
+                b[i][j] = x * 6 // 10 #+ randrange(20) - 10
+            else: 
+                b[i][j] = 0
+
+    #if a[0][0] == 255: f = False
+    #if a[0][0] == 0: f = True
+    #if f: a += 1
+    #else: a -= 1
+
+    a = None
+    b = None
+    img.blit(img2, (0,0))
+
+a = pixels_alpha(img)
+print(a)
+a = None
+
+while 1:
     clock.tick(GLOBAL_FPS)
     window.fill(COLOR_WHITE)
-    #x = img.copy()
+    
+    #img = Surface((200, 300), pygame.SRCALPHA)
+    #img.fill(COLOR_BLACK)
+    effect_step(img)
+
     window.blit(img, (100, 100))
     pygame.display.flip()
     for e in pygame.event.get():
@@ -124,4 +114,4 @@ while 1:
             sys.exit()
         if e.type == pygame.MOUSEBUTTONDOWN and e.button == 1:
             (mx, my) = pygame.mouse.get_pos()
-            (x, y) = the_card.pos
+            print(mx, my)
